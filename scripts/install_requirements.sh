604e3cc76cad9a3ceada3eb435d1497a479828eb
Matthew Carbone <x94carbone@gmail.com>
2023-10-11 14:21:07 -0400

    Add noxfile, remove _easybo

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -2,10 +2,12 @@
 
 pip install toml
 for var in "$@"; do
-    if [[ "$var" == "tests" ]]; then
-        python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["test"]))' | pip install -r /dev/stdin
+    if [[ "$var" == "dev" ]]; then
+        python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["dev"]))' | pip install -r /dev/stdin
     elif [[ "$var" == "docs" ]]; then
-        python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["doc"]))' | pip install -r /dev/stdin
+        python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["docs"]))' | pip install -r /dev/stdin
+    elif [[ "$var" == "extra" ]]; then
+        python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["extra"]))' | pip install -r /dev/stdin
     elif [[ "$var" == "core" ]]; then
         python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip install -r /dev/stdin
     fi
