d7846f3751ff897d5484bdecd98d1f4ead3320d4
Matthew Carbone <x94carbone@gmail.com>
2023-10-11 14:27:05 -0400

    Update pre-commit

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
-import argparse
-import shutil
+# import argparse
+# import shutil
 from pathlib import Path
 
 import nox
 
 DIR = Path(__file__).parent.resolve()
 
-nox.options.sessions = ["lint"] #, "pylint", "tests"]
+nox.options.sessions = ["lint"]  # , "pylint", "tests"]
 
 
 @nox.session
@@ -18,7 +18,11 @@ def lint(session: nox.Session) -> None:
     """
     session.install("pre-commit")
     session.run(
-        "pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs
+        "pre-commit",
+        "run",
+        "--all-files",
+        "--show-diff-on-failure",
+        *session.posargs,
     )
 
 
@@ -45,13 +49,16 @@ def lint(session: nox.Session) -> None:
 # @nox.session(reuse_venv=True)
 # def docs(session: nox.Session) -> None:
 #     """
-#     Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links.
+#     Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check
+# links.
 #     """
 
 #     parser = argparse.ArgumentParser()
-#     parser.add_argument("--serve", action="store_true", help="Serve after building")
+#     parser.add_argument("--serve", action="store_true", help="Serve after
+# building")
 #     parser.add_argument(
-#         "-b", dest="builder", default="html", help="Build target (default: html)"
+#         "-b", dest="builder", default="html", help="Build target (default:
+# html)"
 #     )
 #     args, posargs = parser.parse_known_args(session.posargs)
 
@@ -65,7 +72,8 @@ def lint(session: nox.Session) -> None:
 
 #     if args.builder == "linkcheck":
 #         session.run(
-#             "sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck", *posargs
+#             "sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck",
+# *posargs
 #         )
 #         return
 
@@ -115,4 +123,3 @@ def lint(session: nox.Session) -> None:
 
 #     session.install("build")
 #     session.run("python", "-m", "build")
-
